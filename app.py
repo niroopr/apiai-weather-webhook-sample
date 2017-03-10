e1c703e0d470910edc8637e42427b086e76d27d5
niroopr <niroopr@gmail.com>
2017-03-09 20:13:07 -0800

    Update app.py

--- app.py
+++ app.py
@@ -33,80 +33,28 @@ def webhook():
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
     return r
-
-if req.get("result").get("action") = "finance.stocks":
     
-    def processRequest(req):
-        #if req.get("result").get("action") != "finance.stocks":
-        #    return {}
-        baseurl = "https://query.yahooapis.com/v1/public/yql?"
-        yql_query = makeYqlQuery(req)
-        if yql_query is None:
-            return {}
-        yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback="
-        result = urlopen(yql_url).read()
-        data = json.loads(result)
-        res = makeWebhookResult(data)
-        return res
-
-    def makeYqlQuery(req):
-        result = req.get("result")
-        parameters = result.get("parameters")
-        ticker = parameters.get("company_ticker")
-        if ticker is None:
-            return None
-
-        return "select * from yahoo.finance.quotes where symbol in ('" + ticker + "')"
-
-    def makeWebhookResult(data):
-        query = data.get('query')
-        if query is None:
-            return {}
-
-        result = query.get('results')
-        if result is None:
-            return {}
-
-        quote = result.get('quote')
-        if quote is None:
-            return {}
-
-
-        #speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-                # ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
-        speech = "Last Trade Price is " + quote.get('LastTradePriceOnly')
-        print("Response:")
-        print(speech)
-
-        return {
-            "speech": speech,
-            "displayText": speech,
-            # "data": data,
-            # "contextOut": [],
-            "source": "apiai-weather-webhook-sample"
-        }
-
-elif req.get("result").get("action") = "finance.stocks":
-        baseurl = "https://query.yahooapis.com/v1/public/yql?"
+def processRequest(req):
+    if req.get("result").get("action") != "finance.stocks":
+        return {}
+    baseurl = "https://query.yahooapis.com/v1/public/yql?"
     yql_query = makeYqlQuery(req)
     if yql_query is None:
         return {}
-    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
+    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback="
     result = urlopen(yql_url).read()
     data = json.loads(result)
     res = makeWebhookResult(data)
     return res
 
-
 def makeYqlQuery(req):
     result = req.get("result")
     parameters = result.get("parameters")
-    city = parameters.get("geo-city")
-    if city is None:
+    ticker = parameters.get("company_ticker")
+    if ticker is None:
         return None
 
-    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
-
+    return "select * from yahoo.finance.quotes where symbol in ('" + ticker + "')"
 
 def makeWebhookResult(data):
     query = data.get('query')
@@ -117,25 +65,14 @@ def makeWebhookResult(data):
     if result is None:
         return {}
 
-    channel = result.get('channel')
-    if channel is None:
+    quote = result.get('quote')
+    if quote is None:
         return {}
 
-    item = channel.get('item')
-    location = channel.get('location')
-    units = channel.get('units')
-    if (location is None) or (item is None) or (units is None):
-        return {}
 
-    condition = item.get('condition')
-    if condition is None:
-        return {}
-
-    # print(json.dumps(item, indent=4))
-
-    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
-
+    #speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
+            # ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
+    speech = "Last Trade Price is " + quote.get('LastTradePriceOnly')
     print("Response:")
     print(speech)
 
@@ -146,8 +83,7 @@ def makeWebhookResult(data):
         # "contextOut": [],
         "source": "apiai-weather-webhook-sample"
     }
-else:
-    return {}
+
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
 
