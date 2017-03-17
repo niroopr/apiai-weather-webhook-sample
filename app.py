43be5f22d9c16961388fa153b89192bbf565fb20
niroopr <niroopr@gmail.com>
2017-03-16 22:21:21 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,7 @@ from __future__ import print_function
 from future.standard_library import install_aliases
 install_aliases()
 
-from urllib.parse import urlparse, urlencode
-from urllib.request import urlopen, Request
-from urllib.error import HTTPError
-
+import suds
 from suds.client import Client
 from suds.sudsobject import asdict
 
