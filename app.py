ae93d43d12b8e49ba077efc6369eb358880e3999
niroopr <niroopr@gmail.com>
2017-03-18 11:47:37 -0700

    Update app.py

--- app.py
+++ app.py
@@ -19,16 +19,17 @@ from flask import make_response
 app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
-#def webhook():
-req = request.get_json(silent=True, force=True)
-print("Request:")
-print(json.dumps(req, indent=4))
-res = processRequest(req)
-res = json.dumps(res, indent=4)
-# print(res)
-r = make_response(res)
-r.headers['Content-Type'] = 'application/json'
-return r 
+def webhook():
+##    req = request.get_json(silent=True, force=True)
+##    print("Request:")
+##    print(json.dumps(req, indent=4))
+##    res = processRequest(req)
+##    res = json.dumps(res, indent=4)
+##    # print(res)
+##    r = make_response(res)
+    r = "Niroop is a good guy"
+    r.headers['Content-Type'] = 'application/json'
+    return r 
 def processRequest(req):
     if req.get("result").get("action") != "finance.stocks":
         return {}
